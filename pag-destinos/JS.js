961ccefe27a894180c1979dc595f8368515024ce
AlejandroPS <alejandroporras248@gmail.com>
2022-04-29 15:58:33 -0600

    Arreglo en Contactanos añadido

--- pag-destinos/JS.js
+++ pag-destinos/JS.js
@@ -1,13 +1,13 @@
-let loadMoreBtn = document.querySelector('.paquetes .cargar-mas .btn');
+let CargarMasBtn = document.querySelector('.paquetes .cargar-mas .btn');
 let currentItem = 3;
 
-loadMoreBtn.onclick = () =>{
+CargarMasBtn.onclick = () =>{
    let boxes = [...document.querySelectorAll('.paquetes .container-lugares .box')];
    for (var i = currentItem; i < currentItem + 3; i++){
       boxes[i].style.display = 'inline-block';
    };
    currentItem += 3;
    if(currentItem >= boxes.length){
-      loadMoreBtn.style.display = 'none';
+      CargarMasBtn.style.display = 'none';
    }
 }
